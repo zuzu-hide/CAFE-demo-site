a109da83de73f3a05e85a6ac88d8c07d40107f42
zuzu-hide <hide.d.kazu@gmail.com>
2026-01-18 13:50:04 +0800

    initial cafe site commit

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ hbgMenu.addEventListener(`click`, function() {
     hbgMenu.classList.toggle(`active`);
     hbgContents.classList.toggle(`open`);
 })
-const hgmenuBackground = document
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+function changeSlide() {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+setInterval(changeSlide, 4000)
