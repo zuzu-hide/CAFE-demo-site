1452c7b5749aa08c7f96dbcba0ff8791ba5446b0
zuzu-hide <hide.d.kazu@gmail.com>
2026-01-20 17:41:24 +0800

    update

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
+//hamburgerMenu
+/*
 const hbgMenu = document.getElementById(`hbgMenu`);
 const hbgContents = document.getElementById(`hbgContents`);
 hbgMenu.addEventListener(`click`, function() {
     hbgMenu.classList.toggle(`active`);
     hbgContents.classList.toggle(`open`);
+    document.body.style.overflow = `hidden`
 })
+const hbgDisplay = document.getElementById('hbgDisplay');
+hbgDisplay.addEventListener('click', function() {
+    hbgMenu.classList.remove(`active`);
+    hbgContents.classList.remove(`open`);
+    document.body.style.overflow = ``
+});
+*/
+
+function openMenu() {
+    hbgMenu.classList.add('active');
+    hbgContents.classList.add('open');
+    hbgDisplay.classList.add(`open`);
+}
+function closeMenu() {
+    hbgMenu.classList.remove('active');
+    hbgContents.classList.remove('open');
+    hbgDisplay.classList.remove(`open`);
+}
+hbgMenu.addEventListener('click', () => {
+    hbgContents.classList.contains('open') ? closeMenu() : openMenu();
+});
+hbgDisplay.addEventListener('click', closeMenu);
+
+
 const slides = document.querySelectorAll('.slide');
 let currentSlide = 0;
 function changeSlide() {
